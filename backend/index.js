0459572a02d44208b906eb17d2f07f783318136c
Ben Lague <ben@benlague.com>
2021-03-18 17:38:50 -0400

    forgot to add index.js... :)

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 3000
-require('dotenv').config()
+const db=require('./db')
 
-const admin = require('firebase-admin');
-const serviceAccount = require(process.env.APP_CREDS);
-
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
-  });
-
-const db = admin.firestore();
-
-app.get('/', async (req, res) => {
-    const snapshot = await db.collection('Recipes').get();
-    console.log(snapshot)
-    snapshot.forEach((doc) => {
-      console.log(doc.id, '=>', doc.data());
-    });    
-    res.send('Hello World!')
+app.get('/', (req, res) => {  
+  db.getrecipes()
+  res.send('Hello World!')
 })
 
 app.listen(port, () => {
