8078d9a2b00a97983fa216ea61afaa891fbccd35
Ning Wang <sning.wang@outlook.com>
2021-11-22 15:40:09 -0500

    Updates

--- frontend/src/pages/signup/store/actionCreators.js
+++
@@ -1,6 +0,0 @@
-import * as constants from './constants';
-
-export const logout = () => ({
-    type: constants.LOGOUT,
-    value: false
-})
--- frontend/src/store/reducer.js
+++
@@ -1,9 +0,0 @@
-import{ combineReducers} from 'redux-immutable';
-import{ reducer as signupReducer } from '../pages/signup/store';
-import{ reducer as loginReducer } from '../pages/login/store';
-const reducer = combineReducers({
-    signup: signupReducer,
-    login: loginReducer
-})
-
-export default reducer;
